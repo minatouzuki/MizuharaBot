87b9f5086461a0037c1a54e3a86bf7527432a8e0
minatouzuki <72920726+minatouzuki@users.noreply.github.com>
2021-01-09 01:52:37 +0530

    Update anime.py

--- Mizuhararobot/modules/anime.py
+++ Mizuhararobot/modules/anime.py
@@ -442,7 +442,6 @@ async def kaizoku(c: Client, update: Update):
     await site_search(c, update, "kaizoku")
 
 
-#
 @pbot.on_message(filters.command('kayo'))
 async def kayo(c: Client, update: Update):
     await site_search(c, update, "kayo")
